79b8f1e9a28fc24ce1f415e8442335ea4801df90
equick <edwardquick@hotmail.com>
2015-03-29 15:29:41 +0100

    fixed issue #1

--- modules/custom/ext.nagios.jquery.atips.js
+++ modules/custom/ext.nagios.jquery.atips.js
@@ -1,7 +1,8 @@
 //This invokes the cluetips functionality that comes with pnp4nagios after a page loads
 //and persists it with every refresh
-$( document ).on( "mouseover", "a.tips", function() {
-	jQuery(document).ready(function() {
-		jQuery('a.tips').cluetip({ajaxCache: false, dropShadow: false,showTitle: false });
-	});
+$(".status").on("mouseover", function () {
+        jQuery.noConflict();
+        jQuery(document).ready(function() {
+                jQuery('a.tips').cluetip({ajaxCache: false, dropShadow: false,showTitle: false });
+        });
 });
